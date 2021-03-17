2611b7e6c156c06284ccf1fc7a5aeece11c09596
Chris Keller <ckeller191@gmail.com>
2021-03-17 15:35:42 -0500

    end of class 3/17/21

--- nlp_1.py
+++ nlp_1.py
@@ -11,4 +11,71 @@ blob = TextBlob(text)
 #print(blob.words)
 
 
-print(blob.tags)
+#print(blob.tags)
+
+#print(blob.noun_phrases)
+
+#print(blob.sentiment)
+
+#print(round(blob.sentiment.polarity,3))
+
+#print(round(blob.sentiment.subjectivity,3))
+
+sentences = blob.sentences
+
+"""
+for sentence in sentences:
+    print(sentence)
+    print(sentence.sentiment)
+    #can use round on the ones below
+    print(sentence.sentiment.polarity)
+    print(sentence.sentiment.subjectivity)
+"""
+
+
+from textblob.sentiments import NaiveBayesAnalyzer
+
+blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+
+"""
+print(blob.sentiment)
+
+for sentence in blob.sentences:
+    print(blob.sentiment)
+
+blob.detect_language()   #detect the language of the text - should return 'en'
+
+spanish = blob.translate(to='es')
+
+print(spanish)
+"""
+
+from textblob import Word
+"""
+index = Word('index')
+
+print(index.pluralize())
+
+cacti = Word('cacti')
+
+print(cacti.singularize())
+
+
+animals = TextBlob('dog cat fish bird').words
+
+print(animals.pluralize())
+
+"""
+
+word = Word('theyr')
+
+print(word.spellcheck())
+
+corrected_word = word.correct()
+
+sentence = TextBlob('Ths sentense has missplle wrds')
+
+corrected_sentence = sentence.correct()
+
+print(corrected_word)
+print(corrected_sentence)
