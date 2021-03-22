04e8d9c4b2176fbf8711a99258c869d5ef5dff73
Chris Keller <ckeller191@gmail.com>
2021-03-22 15:45:44 -0500

    end of class 3/22

--- nlp_2.py
+++ nlp_2.py
@@ -21,6 +21,44 @@ print(blob.words.count("joy"))
 print(blob.noun_phrases.count("lady capulet"))
 """
 
-stops = stopwords.words("engligh")
+stops = stopwords.words("english")
+
+more_stops = ['thee', 'thy', 'thou']
+
+stops += more_stops
 
 items = blob.word_counts.items()
+#print(items)
+
+clean_items = [item for item in items if item[0] not in stops]
+
+#print(clean_items)
+
+
+#Sorting natively
+
+from operator import itemgetter
+
+sorted_items = sorted(clean_items)
+#print(sorted_items[:10])
+
+#using itemgetter
+
+sorted_items = sorted(clean_items, key=itemgetter(1), reverse=True)
+#print(sorted_items[:10])
+
+top20 = sorted_items[:20]
+
+print(top20)
+
+df = pd.DataFrame(top20, columns=["word", "count"])
+
+print(df)
+
+import matplotlib.pyplot as plt
+
+df.plot.bar(x="word", y="count", rot=0, legend=False, color=["y", "c", "m", "b", "g", "r"])
+
+plt.gcf().tight_layout
+
+plt.show()
