92cead7680532b27fcfb9da8ab6da22dcc3b5c0a
Chris Keller <ckeller191@gmail.com>
2021-03-22 15:27:45 -0500

    download files from canvas

--- nlp_1.py
+++ nlp_1.py
@@ -48,10 +48,47 @@ blob.detect_language()   #detect the language of the text - should return 'en'
 spanish = blob.translate(to='es')
 
 print(spanish)
+
+german = blob.translate(to="de")
+
+print(german)
+
+chinese = blob.translate(to="zh-CN")
+
+print(chinese)
 """
 
 from textblob import Word
+
+word1 = Word("studies")
+word2 = Word("varieties")
+
+#print(word1.lemmatize())
+#print(word2.lemmatize())
+
+happy = Word("happy")
+
+#print(happy.definitions)
 """
+for synset in happy.synsets:
+    print(synset)
+    for lemma in synset.lemmas():
+        print(lemma)
+        print(lemma.name())
+
+
+lemmas = happy.synsets[0].lemmas()
+print(lemmas)
+
+for lemma in lemmas[0].antonyms():
+    print(lemma.name())
+
+
+
+
+
+
+
 index = Word('index')
 
 print(index.pluralize())
@@ -65,7 +102,7 @@ animals = TextBlob('dog cat fish bird').words
 
 print(animals.pluralize())
 
-"""
+
 
 word = Word('theyr')
 
@@ -79,3 +116,20 @@ corrected_sentence = sentence.correct()
 
 print(corrected_word)
 print(corrected_sentence)
+"""
+
+import nltk
+
+#nltk.download("stopwords") - only have to run once
+
+from nltk.corpus import stopwords
+
+stops = stopwords.words("english")
+
+#print(stops)
+
+blob = TextBlob("Today is a beautiful day")
+
+no_stops = [word for word in blob.words if word not in stops]
+
+print(no_stops)
--- nlp_2.py
+++ nlp_2.py
@@ -6,3 +6,21 @@ from nltk.corpus import stopwords
 from pathlib import Path
 
 import pandas as pd
+
+blob = TextBlob(Path("RomeoAndJuliet.txt").read_text())
+
+"""
+print(blob.word_counts["juliet"])
+
+print(blob.word_counts["romeo"])
+
+print(blob.word_counts["thou"])
+
+print(blob.words.count("joy"))
+
+print(blob.noun_phrases.count("lady capulet"))
+"""
+
+stops = stopwords.words("engligh")
+
+items = blob.word_counts.items()
